f62a191930046c6705932ac44a63e8c4e9131e58
jongking <hynb14@gmail.com>
2022-12-21 04:17:27 +0900

    1152_단어의개수_221221.cpp 추가

---
+++ boj/1152_단어의개수_221221.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+string t;
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int ans = 0;
+	while (cin >> t) ans++;
+	cout << ans;
+}
