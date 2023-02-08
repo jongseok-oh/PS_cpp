7988977673fad4f3d09cda4c3a7b07194986d55e
jongking <hynb14@gmail.com>
2023-02-08 23:51:34 +0900

    11382_꼬마정민_230208.cpp 추가

---
+++ boj/11382_꼬마정민_230208.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+long long a, b, c;
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	cin >> a >> b >> c;
+	cout << a + b + c;
+}
