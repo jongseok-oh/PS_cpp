ba0ea54164ecfb5b62430f65f94e5f0e8fbde6ae
jongseok-oh <hynb14@gmail.com>
2023-05-18 17:11:36 +0900

    [leetcode] 1557. Minimum Number of Vertices to Reach All Nodes_230518.cpp 추가

---
+++ leetcode/1557. Minimum Number of Vertices to Reach All Nodes_230518.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
+        vector<int> inDegree(n);
+
+        for(auto& edge: edges){
+            inDegree[edge[1]]++;
+        }
+
+        vector<int> ans;
+
+        for(int i = 0; i<n; i++)
+            if(!inDegree[i]) ans.push_back(i);
+
+        return ans;
+    }
+};
