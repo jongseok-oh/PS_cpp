32badec92301050a513e006d24dbc36efbfae1bf
JongKing <hynb14@gmail.com>
2023-06-19 18:58:12 +0900

    [boj] 1647_도시 분할 계획_데이터추가_230619.cpp 추가

---
+++ boj/1647_도시 분할 계획_데이터추가_230619.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n, m;
+
+struct Road {
+	int a, b, cost;
+}roads[1000000];
+
+int parent[100001];
+
+int getP(int t) {
+	if (!parent[t]) return t;
+	return parent[t] = getP(parent[t]);
+}
+
+bool merge(int a, int b) {
+	int aP = getP(a);
+	int bP = getP(b);
+
+	if (aP == bP) return false;
+
+	parent[aP] = bP;
+	return true;
+}
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	cin >> n >> m;
+
+	if (n == 2) { cout << 0; return 0; }
+
+	for (int i = 0; i < m; i++)
+		cin >> roads[i].a >> roads[i].b >> roads[i].cost;
+
+	sort(roads, roads + m, [](const Road& a, const Road& b)
+	{return a.cost < b.cost; }
+	);
+
+	int con = 0, ans = 0;
+
+	for (int i = 0; i < m; i++) {
+		if (merge(roads[i].a, roads[i].b)) {
+			ans += roads[i].cost;
+			con++;
+			if (con == n - 2) break;
+		}
+	}
+	cout << ans;
+}
