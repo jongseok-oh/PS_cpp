e06b9daa94299942032d82601a6c2491a8bf2549
jongking <hynb14@gmail.com>
2023-06-21 02:04:20 +0900

    [boj] 13977_이항 계수와 쿼리_속도 개선_230621.cpp 추가

---
+++ boj/13977_이항 계수와 쿼리_속도 개선_230621.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int facto[4000001];
+
+const int MOD = 1e9 + 7;
+int n, k;
+
+int _pow(int a, int t) {
+	
+	int ret = 1;
+	
+	while (t) {
+		if (t & 1) ret = 1LL * ret * a % MOD;
+
+		a = 1LL * a * a % MOD;
+		t >>= 1;
+	}
+
+	return ret;
+}
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+	facto[0] = 1;
+	facto[1] = 1;
+
+	for (int i = 2; i < 4000001; i++) facto[i] = 1LL * facto[i - 1] * i % MOD;
+
+	int m; cin >> m;
+	while (m--) {
+		cin >> n >> k;
+
+		cout << (1LL * facto[n] * _pow(1LL * facto[n - k] * facto[k] % MOD, MOD - 2) % MOD) << "\n";
+	}
+}
