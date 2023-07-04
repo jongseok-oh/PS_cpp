b756bbb2d20f37387fdeb7df444beb07936f9510
jongking <hynb14@gmail.com>
2023-07-04 18:10:48 +0900

    [leetcode] 137. Single Number II_230704.cpp 추가

---
+++ leetcode/137. Single Number II_230704.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int, int> m;
+        for(int& num: nums) m[num]++;
+
+        for(auto it = m.begin(); it != m.end(); it++)
+            if(it->second == 1) return it->first;
+        
+        return -1;
+    }
+};
