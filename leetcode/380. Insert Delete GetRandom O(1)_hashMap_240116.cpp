be36f7f0c330d1377bdb6d5882e13fa6065ad550
jongseok-oh <hynb14@gmail.com>
2024-01-16 19:55:58 +0900

    [leetcode] 380. Insert Delete GetRandom O(1)_hashMap_240116.cpp 추가

---
+++ leetcode/380. Insert Delete GetRandom O(1)_hashMap_240116.cpp
@@ -0,0 +1,34 @@
+class RandomizedSet {
+    vector<int> v;
+    unordered_map<int, int> cache;
+public:
+    RandomizedSet() {}
+    
+    bool insert(int val) {
+        if(cache.count(val)) return false;
+        v.push_back(val);
+        cache[val] = v.size()-1;
+        return true;
+    }
+    
+    bool remove(int val) {
+        if(!cache.count(val)) return false;
+        v[cache[val]] = v.back();
+        cache[v.back()] = cache[val];
+        v.pop_back();
+        cache.erase(val);
+        return true;
+    }
+    
+    int getRandom() {
+        return v[rand()%v.size()];
+    }
+};
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet* obj = new RandomizedSet();
+ * bool param_1 = obj->insert(val);
+ * bool param_2 = obj->remove(val);
+ * int param_3 = obj->getRandom();
+ */
