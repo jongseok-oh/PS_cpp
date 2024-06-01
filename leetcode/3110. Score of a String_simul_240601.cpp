5dabb6a27972dff405bc1c8f42c64bd0da5ef084
jongking <hynb14@gmail.com>
2024-06-01 20:31:18 +0900

    [leetcode] 3110. Score of a String_simul_240601.cpp 추가

---
+++ leetcode/3110. Score of a String_simul_240601.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int scoreOfString(string s) {
+        int ret = 0;
+        for(int i = 0; i<s.size()-1; ++i)
+            ret += abs(s[i] - s[i+1]);
+        return ret;
+    }
+};
