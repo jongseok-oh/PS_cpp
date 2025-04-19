b7d0c028c0532505c6c5e4bae10c872f928876f8
jongseok-oh <hynb14@gmail.com>
2025-04-19 15:53:26 +0900

    mac용 vscode 디버깅 세팅

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+    int n, m;
+    cin >> n >> m;
+    cout << n << " " << m << "\n";
+    cout << n << " " << m << "\n";
+}
